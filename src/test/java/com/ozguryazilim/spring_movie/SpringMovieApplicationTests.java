07e459cd03bc7a1c8a0862a8bc8e54acb027e7aa
Tarık <retatu@outlook.com>
2021-04-08 21:43:14 +0300

    Initial commit

---
+++ src/test/java/com/ozguryazilim/spring_movie/SpringMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozguryazilim.spring_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
